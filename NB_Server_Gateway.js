e4e3fc3d6d4212c0d336f6ad8092f7cb33c14bd9
MoustiriAbdelali <abdelali.kb19@gmail.com>
2024-02-05 21:15:44 +0100

    chnager les heure

--- NB_Server_Gateway.js
+++ NB_Server_Gateway.js
@@ -272,7 +272,7 @@ function convertToHexadecimal(number,linght) {
 var now = new Date();
     var value   = now.getHours();
 
-if (value===9 || value===10 || value===11 ||value===12 ||value===13 ||value===14 ||value===15 ||value===16 ||value===17  ) {
+if (value===8 ||  value===9 || value===10 || value===11 ||value===12 ||value===13 ||value===14 ||value===15 ||value===16 ||value===17 ||value===18  ) {
 return false
     
 }else{
